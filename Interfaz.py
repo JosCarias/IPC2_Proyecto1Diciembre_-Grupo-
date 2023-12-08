fc537ff3bb2e6a0d003717e849f1b54c62a9c5a5
Josue Ricardo Carias Ordoñez <3661044620101@ingenieria.usac.edu.gt>
2023-12-08 08:32:11 -0600

    Inicio interfaz
    sigo en la tarde =)

---
+++ Interfaz.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# Crear la menu principal
+menu = tk.Tk()
+
+# Opcional: Establecer título y tamaño de la menu
+menu.title("Spotify Piraton ")
+menu.geometry("720x480") 
+menu.resizable(False, False) 
+
+# Mostrar la menu
+menu.mainloop()
